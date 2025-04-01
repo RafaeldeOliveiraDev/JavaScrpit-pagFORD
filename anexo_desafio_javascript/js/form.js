9b33fb85031c0667aeba265d88b723eef8add1ab
Rafael de Oliveira Carvalho <rdeoliveiracarvalhoo@gmail.com>
2025-04-01 10:13:55 -0300

    Add files via upload

--- anexo_desafio_javascript/js/form.js
+++ anexo_desafio_javascript/js/form.js
@@ -1,44 +1,46 @@
-
-//class contato
-
+// Classe Contato
 class Contato {
-
-    constructor(nome, sobrenome, email, cpf, telefone, contato){
-        this.nome = nome
-        this.sobrenome = sobrenome
-        this.email = email
-        this.cpf = cpf
-        this.telefone = telefone
-        this.contato = contato
+    constructor(nome, sobrenome, email, cpf, telefone, contato) {
+        this.nome = nome;
+        this.sobrenome = sobrenome;
+        this.email = email;
+        this.cpf = cpf;
+        this.telefone = telefone;
+        this.contato = contato;
     }
-
 }
 
-function Post(form){
+function Post(event, form) {
+
+    // Coletando os dados
     const nome = form.nome.value;
-    const sobrenome = form.nome.value;
+    const sobrenome = form.sobrenome.value;
     const email = form.email.value;
     const cpf = form.cpf.value;
     const telefone = form.telefone.value;
     const contato = form.contato.value;
 
+    // Verifique se os dados estão sendo coletados corretamente
+    console.log("Nome:", nome);
+    console.log("Sobrenome:", sobrenome);
+    console.log("Email:", email);
+    console.log("CPF:", cpf);
+    console.log("Telefone:", telefone);
+    console.log("Contato:", contato);
+
+    // Criando um objeto Contato com os dados
     let data = new Contato(nome, sobrenome, email, cpf, telefone, contato);
 
-    Enviar(nome)
+    event.preventDefault(); // Previne o recarregamento da pagina do formulário
 
-    Event.preventDefault();
+    // Chama a função Enviar
+    Enviar(nome);
 }
 
 function Enviar(nome) {
-
-    let nome = document.getElementById("nome").value;
-
-    if (nome.value == "") {
-       alert('Por favor, preencha todos os campos!');
-       return;
-    
-    }else{
-       alert('Obrigado sr(a) ' + nome + ' os seus dados foram encaminhados com sucesso');
+    if (nome == "") {
+        alert('Por favor, preencha todos os campos!');
+    } else {
+        alert('Obrigado ' + nome + ', os seus dados foram encaminhados com sucesso!');
     }
-  
 }
